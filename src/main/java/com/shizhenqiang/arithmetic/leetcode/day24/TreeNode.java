7e1b379e897bc18dc86f55fbef168ef053003b98
shizhenqiang <15652182014@163.com>
2020-07-16 15:52:25 +0800

    实现二叉树打印和二叉树找到最近公共祖先

---
+++ src/main/java/com/shizhenqiang/arithmetic/leetcode/day24/TreeNode.java
@@ -0,0 +1,12 @@
+package com.shizhenqiang.arithmetic.leetcode.day24;
+
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
